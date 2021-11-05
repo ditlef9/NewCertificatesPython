b1fc3e0230e77cee6ea43ca57265c3a4ba02c305
Sindre <siditlef@gmail.com>
2021-11-05 12:40:10 +0100

    x

--- DBAdapter.py
+++ DBAdapter.py
@@ -1,3 +1,14 @@
+###
+#
+# File: DBAdapter.py
+# Version 1.0.0
+# Date 14:28 25.03.2021
+# Copyright (c) 2021 Sindre Andre Ditlefsen
+# License: http://opensource.org/licenses/gpl-license.php GNU Public License
+#
+###
+
+
 import mysql.connector
 from mysql.connector import errorcode
 
--- Main.py
+++ Main.py
@@ -1,4 +1,12 @@
-# This is a sample Python script.
+###
+#
+# File: Main.py
+# Version 1.0.0
+# Date 14:28 25.03.2021
+# Copyright (c) 2021 Sindre Andre Ditlefsen
+# License: http://opensource.org/licenses/gpl-license.php GNU Public License
+#
+###
 
 import logging
 import sys
